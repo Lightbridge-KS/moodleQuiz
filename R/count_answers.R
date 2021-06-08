74ce7cd136fd4ac1814951c955feb2cf84473f38
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-06-08 20:57:47 +0700

    Add count answers from single & multi MCQ

--- R/count_answers.R
+++ R/count_answers.R
@@ -1,7 +1,56 @@
 ### Count Answers from Response Columns
 
-# Count Cloze Answers ------------------------------------------------------
 
+# Count Single Answered MCQ -----------------------------------------------
+
+
+#' Count Answers from Single Answered type question
+#'
+#' @param data A data.frame of Moodle Responses report
+#' @param col (quoted or unquoted) A column that contain Single Answered type question
+#' @param sort (Logical) sorted or not
+#'
+#' @return A data.frame
+#'
+count_answers_single_col <- function(data,
+                                     col,
+                                     sort = TRUE) {
+
+  col <- dplyr::ensym(col)
+  data %>%
+    dplyr::count(!!col, sort = sort) %>%
+    dplyr::mutate(Percent = (n/sum(n))*100)
+}
+
+# Count Multi-Answered MCQ ------------------------------------------------
+
+
+#' Count Answers from Multiple Answered type question
+#'
+#' @param data A data.frame of Moodle Responses report
+#' @param col (quoted or unquoted) A column that contain Multiple Answered type question
+#' @param sort (Logical) sorted or not
+#'
+#' @return A data.frame
+#'
+count_answers_multi_col <- function(data,
+                                    col,
+                                    sort = TRUE
+) {
+
+  col <- dplyr::ensym(col)
+  data %>%
+    dplyr::count(!!col, sort = sort) %>%
+    # Separate
+    tidyr::separate_rows(!!col, sep = "(\\r)*\\n;\\s*") %>%
+    # Second Count
+    dplyr::count(!!col, wt = n, sort = sort) %>%
+    # Percent
+    dplyr::mutate(Percent = (n/sum(n))*100)
+
+}
+
+# Count Cloze Answers ------------------------------------------------------
 
 #' Count Answers from Cloze column
 #'
