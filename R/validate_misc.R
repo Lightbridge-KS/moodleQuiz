db636656e0e46e959a4a834a4dc93eaca160cf84
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-06-08 20:38:55 +0700

    Add validate: Is it a Multi-answerd MCQ column ?

---
+++ R/validate_misc.R
@@ -0,0 +1,20 @@
+
+
+# Is a multi-response colum ? -----------------------------------------------
+
+
+
+#' Is a Column contained multiple answered MCQ
+#'
+#' @param data A data.frame of Moodle Responses report
+#' @param col (tidy-select) Column to test
+#'
+#' @return Named logical vector: `TRUE` if a column is a multiple answered MCQ
+#'
+is_multi_resp_col <- function(data, col) {
+
+  col <- rlang::enquo(col)
+  data %>%
+    is_regex_in_cols(!!col, regex = "(\\r)*\\n;\\s*", match_rows = "any")
+
+}
