2eac4df7377671f51dde0d17473f1ea5b11db017
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-06-09 21:53:48 +0700

    add "n"

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 ### Global variable
 
 utils::globalVariables(c("Email address", "Started", "Email",
-                         "Name", "State", "Encode", "ID"))
+                         "Name", "State", "Encode", "ID", "n"))
