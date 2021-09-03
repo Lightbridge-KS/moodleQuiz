6465c416067931c230a4b92201705855f0a4eeab
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-09-03 17:08:41 +0700

    Add Department, Institution

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 ### Global variable
 
-utils::globalVariables(c("Email address", "Started", "Email",
+utils::globalVariables(c("Email address", "Started", "Email","Department", "Institution",
                          "Name", "State", "Encode", "ID", "n"))
