28874c0211a6e214785c7c30362541bf245ab76a
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-04-19 22:35:40 +0700

    add global var "ID number"

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,5 @@
 ### Global variable
 
-utils::globalVariables(c("Email address", "Started", "Email","Department", "Institution",
+utils::globalVariables(c("Email address", "Started", "Email",
+                         "Department", "Institution", "ID number",
                          "Name", "State", "Encode", "ID", "n"))
